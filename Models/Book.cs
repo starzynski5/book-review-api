fd852f237fa3b029713bf9eb7eab426554572271
youngplq10 <patryk.plq@o2.pl>
2025-07-12 12:18:14 +0200

    Created book model

---
+++ Models/Book.cs
@@ -0,0 +1,17 @@
+﻿namespace book_review_api.Models
+{
+    public class Book
+    {
+        private readonly int Id;
+
+        private string Title { get { return Title; } set { Title = value; } }
+
+        private string Description { get { return Description; } set { Description = value; } }
+
+        private string Author {  get { return Author; } set { Author = value; } }
+
+        private int ReleaseYear { get { return ReleaseYear; } set { ReleaseYear = value; } }
+
+        //list of reviews
+    }
+}
